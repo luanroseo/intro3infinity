40aa6ef50aa7d778279b1733b7e67a09df622345
luanroseo <150529481+luanroseo@users.noreply.github.com>
2023-11-18 10:17:51 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,76 @@
+//PRIMEIRA QUESTÃO Utilizando while/for imprima 10 vezes a mensagem "Olá, Mundo!"
+/*
+let n = 1
+while(n <= 10){
+    n++
+    console.log("Hello World!")
+}
+*/
+
+//SEGUNDA QUESTÃO Utilize while/for para imprimir uma sequência de 0 a 100.
+/*
+for(let n = 0; n<101;  n++){
+    console.log(n)
+}
+*/
+
+//TERCEIRA QUESTÃO Utilize while/for para imprimir uma sequência de 0 até um número digitado pelo usuário
+/*
+let num = Number(prompt("Digite um número"))
+for(let acumulador = 0; acumulador < num; acumulador++){
+console.log(acumulador)
+}
+
+
+let num = Number(prompt("Digite um número"))
+for(let numfor = 0; numfor <= num; numfor++){
+console.log(numfor)
+}
+*/
+
+// const numeroDigitado = Number(prompt("Digite um número"))
+// let acumulador = 0
+// while(acumulador <= numeroDigitado){
+//     console.log(acumulador)
+//     acumulador++
+// }
+
+//QUARTA QUESTÃO Utilize while/for para imprimir todos os pares entre 1 e o número digitado pelo usuário.
+
+// const numero = Number(prompt("Digite um número:"))
+// let acc = 1
+// while(acc <= numero){
+//     if(acc % 2 === 1)
+//     {
+//         console.log(acc)
+//     }
+//     acc++
+// }
+
+// const numeroDigitado = Number(prompt("Digite um numero:"))
+
+// for(let acc = 1; acc <= numeroDigitado; acc++){
+//     if(acc % 2 === 0){
+//         console.log(acc)
+//     }
+// }
+
+//QUINTA QUESTÃO Escreva um programa que solicite um número inteiro positivo e faça uma contagem regressiva a partir desse número até 0
+
+// let numeroInteiro = Number(prompt("Digite um número inteiro positivo"))
+// while(numeroInteiro >= 0){
+//         console.log(numeroInteiro)
+//         numeroInteiro--
+        
+// }
+
+// let numeroInteiro = Number(prompt("Digite um número inteiro positivo"))
+// for(let i = numeroInteiro; i >= 0; i--){
+//     console.log(i)
+// }
+
+
+
+//QUESTÃO DE AULA
+
+//https://drive.google.com/drive/folders/1C2KkoCLNf0ePl5gxpe91hg5RSB0TesOS
